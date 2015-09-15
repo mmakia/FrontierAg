48651967f0a332e67eeca5e88b9e1dbf2ca66f08
mmakia@outlook.com <mmakia@outlook.com>
2015-09-15 13:28:51 -0400

    DidChangesButNotToProcessYet

--- FrontierAg/Checkout/CheckoutComplete.aspx.cs
+++ FrontierAg/Checkout/CheckoutComplete.aspx.cs
@@ -13,12 +13,14 @@ namespace FrontierAg.Checkout
 {
     public partial class CheckoutComplete : System.Web.UI.Page
     {
-        string isSendEmails = ConfigurationManager.AppSettings["SendEmails"];
+        string isSendStaffEmails = ConfigurationManager.AppSettings["SendEmails"];        
 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
+                string ItemsOrdered = getItemsOrdered(HttpContext.Current.Session["OrderId"].ToString());
+
                 using (ProductContext db = new ProductContext())
                 {
                     var allEmails = db.Emails.Where(r => r.isForOrder == true);
@@ -27,7 +29,7 @@ namespace FrontierAg.Checkout
                     {
                         Label1.Text = "You just placed a Standing Order which is ordered by " + HttpContext.Current.Session["OrderedBy"].ToString();
 
-                        if (isSendEmails == "1")
+                        if (isSendStaffEmails == "1")
                         {
                             foreach (var a in allEmails)
                             {
@@ -39,13 +41,14 @@ namespace FrontierAg.Checkout
                     {
                         Label1.Text = "You just placed order #" + HttpContext.Current.Session["OrderId"].ToString() + " which is ordered by " + HttpContext.Current.Session["OrderedBy"].ToString();
 
-                        if (isSendEmails == "1")
+                        if (isSendStaffEmails == "1")
                         {
                             foreach (var a in allEmails)
                             {
-                                new Emailer().SendEmail(a.EmailAddress, "orders@frontierssi.com", "FrontierAg New Order #" + HttpContext.Current.Session["OrderId"] + ", Ordered By " + HttpContext.Current.Session["OrderedBy"], "There is a new Order, Please Click on the following link for Details: http://orders2.frontiersci.com/FSIAg/Admin/Orders/OpenOrder");
+                                new Emailer().SendEmail(a.EmailAddress, "orders@frontierssi.com", "FrontierAg New Order #" + HttpContext.Current.Session["OrderId"] + ", Ordered By " + HttpContext.Current.Session["OrderedBy"], "<p style='color: #CC0000;'>Hello,</p>There is a new Order.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");//"There is a new Order, Please Click on the following link for Details: http://orders2.frontiersci.com/FSIAg/Admin/Orders/OpenOrder");
                             }
                         }
+
                         EmailCustomers(HttpContext.Current.Session["OrderId"].ToString());
                     }                    
                 }
@@ -58,9 +61,37 @@ namespace FrontierAg.Checkout
 
         }
 
+
+        public string getItemsOrdered(string stringOrederId)
+        {
+             int orderId = 0;
+
+             if (Int32.TryParse(stringOrederId, out orderId))
+             {
+                 using (ProductContext db = new ProductContext())
+                 {
+                     //getting the items ordered
+                     var ProductNos = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Product.ProductNo).ToArray();
+                     var ProductNames = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Product.ProductName).ToArray();
+                     var Quantities = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Quantity).ToArray();
+                     string strBodyHeader = "<table style='padding:5px 20px 0px 0px' ><tr style='text-decoration:underline;'><td> Product Number </td><td> Product Name </td><td> Quantity </td></tr>";
+                     string strBodyContent = "";
+                     for (int i = 0; i < ProductNos.Length; i++)
+                     {
+                         strBodyContent += "<tr><td>" + ProductNos[i] + "</td><td>" + ProductNames[i] + "</td><td align=\"center\">" + Quantities[i] + "</td></tr>";
+                     }
+                     string strBodyFooter = "</table>";
+                     string messageBody = strBodyHeader + strBodyContent + strBodyFooter;
+                     return messageBody;
+                 }
+             }
+             return "";
+        }
+
         public void EmailCustomers(string stringOrederId)
         {
             int orderId = 0;
+            string ItemsOrdered = getItemsOrdered(HttpContext.Current.Session["OrderId"].ToString());
 
             if (Int32.TryParse(stringOrederId, out orderId))
             {
@@ -72,18 +103,18 @@ namespace FrontierAg.Checkout
                     var ShippingtoshippingId = db.OrderShippings.Where(s => s.OrderId == orderId && s.SType == SType.Shipping).Select(q => q.ShippingId).SingleOrDefault();
                     var ShippingToEmail = db.Shippings.Where(r => r.ShippingId == ShippingtoshippingId).Select(u => u.EMail).SingleOrDefault();
 
-                    //getting the items ordered
-                    var ProductNos = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Product.ProductNo).ToArray();
-                    var ProductNames = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Product.ProductName).ToArray();
-                    var Quantities = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Quantity).ToArray();
-                    string strBodyHeader = "<table style='padding:5px 20px 0px 0px' ><tr style='text-decoration:underline;'><td> Product Number </td><td> Product Name </td><td> Quantity </td></tr>";
-                    string strBodyContent = "";
-                    for (int i = 0; i < ProductNos.Length; i++)
-                    {
-                        strBodyContent += "<tr><td>" + ProductNos[i] + "</td><td>" + ProductNames[i] + "</td><td align=\"center\">" + Quantities[i] + "</td></tr>";
-                    }
-                    string strBodyFooter = "</table>";
-                    string messageBody = strBodyHeader + strBodyContent + strBodyFooter;
+                    ////getting the items ordered
+                    //var ProductNos = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Product.ProductNo).ToArray();
+                    //var ProductNames = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Product.ProductName).ToArray();
+                    //var Quantities = db.OrderDetails.Where(w => w.OrderId == orderId).Select(e => e.Quantity).ToArray();
+                    //string strBodyHeader = "<table style='padding:5px 20px 0px 0px' ><tr style='text-decoration:underline;'><td> Product Number </td><td> Product Name </td><td> Quantity </td></tr>";
+                    //string strBodyContent = "";
+                    //for (int i = 0; i < ProductNos.Length; i++)
+                    //{
+                    //    strBodyContent += "<tr><td>" + ProductNos[i] + "</td><td>" + ProductNames[i] + "</td><td align=\"center\">" + Quantities[i] + "</td></tr>";
+                    //}
+                    //string strBodyFooter = "</table>";
+                    //string messageBody = strBodyHeader + strBodyContent + strBodyFooter;
 
 
                     if (OrderingToEmail != ShippingToEmail)
@@ -91,15 +122,15 @@ namespace FrontierAg.Checkout
                         if (Session["EmailOrderingCustomer"].ToString() != "False")
                         {
                             //email Ordering person
-                            new Emailer().SendEmail(OrderingToEmail, "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>Thank you for shopping with us. You ordered item(s) below.<br /><br /><div style='color: #CC0000;'>Details:</div>" + messageBody + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
-                            new Emailer().SendEmail("mmakia@frontierssi.com", "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>Thank you for shopping with us. You ordered item(s) below.<br /><br /><div style='color: #CC0000;'>Details:</div>" + messageBody + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
+                            new Emailer().SendEmail(OrderingToEmail, "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>Thank you for shopping with us. You ordered item(s) below.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
+                            new Emailer().SendEmail("mmakia@frontierssi.com", "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>Thank you for shopping with us. You ordered item(s) below.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
                         }
 
                         if (Session["EmailShpToCustomer"].ToString() != "False")
                         {
                             //email ShippingTo person
-                            new Emailer().SendEmail(ShippingToEmail, "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>An order has been placed to be shipped to you. The order number is " + orderId + ".<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + messageBody + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
-                            new Emailer().SendEmail("mmakia@frontierssi.com", "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>An order has been placed to be shipped to you. The order number is " + orderId + ".<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + messageBody + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
+                            new Emailer().SendEmail(ShippingToEmail, "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>An order has been placed to be shipped to you. The order number is " + orderId + ".<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
+                            new Emailer().SendEmail("mmakia@frontierssi.com", "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>An order has been placed to be shipped to you. The order number is " + orderId + ".<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
                         }                        
                     }
                     else
@@ -107,8 +138,8 @@ namespace FrontierAg.Checkout
                         if (Session["EmailOrderingCustomer"].ToString() == "True" || Session["EmailShpToCustomer"].ToString() == "True")
                         {
                             //email customer(Ordering person is the same as shippingTo person)
-                            new Emailer().SendEmail(OrderingToEmail, "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>Thank you for shopping with us. You ordered item(s) below.<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + messageBody + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
-                            new Emailer().SendEmail("mmakia@frontierssi.com", "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>An order has been placed to be shipped to you. The order number is " + orderId + ".<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + messageBody + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
+                            new Emailer().SendEmail(OrderingToEmail, "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>Thank you for shopping with us. You ordered item(s) below.<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
+                            new Emailer().SendEmail("mmakia@frontierssi.com", "orders@frontierssi.com", "Frontier Agricultural Sciences Order #" + orderId, "<p style='color: #CC0000;'>Hello,</p>An order has been placed to be shipped to you. The order number is " + orderId + ".<br />You will receive a confirmation email when your order ships.<br /><br /><div style='color: #CC0000;'>Details:</div>" + ItemsOrdered + "<br />Feel free to contact us if you have any question.<br /> http://www.insectrearing.com/contact.html <br /><br /><br />Frontier Agricultural Sciences");
                         }                        
                     }                    
                 }
